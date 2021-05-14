4eaaa0c5c42c0c95f4b6c56e0ced2e53cb01ba1c
JunhwaLee <leejunhwa88@gmail.com>
2021-05-15 00:16:43 +0900

    dependencies 파일 수정

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,11 +3,33 @@
  */
 
 object Versions {
-    const val retrofit_versions = ""
+    const val kotlinVersions = "1.5.0"
+
+    const val rxJava3Versions = "3.0.13-RC4"
+
+    const val retrofitVersions = "2.9.0"
 }
 
 object Deps {
-    const val retrofit2 = "com"
+    // kotlin lib
+    const val kotlinGradlePlugin =
+        "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersions}"
+    const val kotlinSTDLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersions}"
+
+    // rx lib
+    const val rxJava3 = "io.reactivex.rxjava3:rxjava:${Versions.rxJava3Versions}"
+
+    // retrofit lib
+    const val retrofit2 = "com.squareup.retrofit2:retrofit:${Versions.retrofitVersions}"
+    const val retrofit2RxJavaAdapter =
+        "com.squareup.retrofit2:adapter-rxjava3:${Versions.retrofitVersions}"
+    const val retrofit2GsonConverter =
+        "com.squareup.retrofit2:converter-gson:${Versions.retrofitVersions}"
+
+    // unit test lib
+    const val testKotlinJDK8 = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlinVersions}"
+    const val testKotlin = "org.jetbrains.kotlin:kotlin-test:${Versions.kotlinVersions}"
+    const val testKotlinJunit = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlinVersions}"
 }
 
 object BuildConfig {
